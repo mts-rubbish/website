f4344af5cfefdc8397dee7f9115924378354965c
mts-rubbish <mts_rubbish@163.com>
2026-03-03 14:41:02 +0000

    Delete Post Draft source/_drafts/投稿说明.md by Qexo 由Github操作更新

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/20/slice/"];function toRandomPost(){
+var posts=["2024/04/20/slice/","2026/03/03/投稿说明/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
