614c04fa4e7a82f5b560532fa93a9ca1ca6f36db
mts-rubbish <mts_rubbish@163.com>
2026-03-05 08:01:10 +0000

    Update _config.anzhiyu.yml by Qexo 由Github操作更新

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/43751.html","posts/6890.html","posts/62288.html","posts/6147.html"];function toRandomPost(){
+var posts=["posts/6890.html","posts/43751.html","posts/62288.html","posts/6147.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
