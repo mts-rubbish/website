f80466c6d0eb7df3df92a5c36860669d3d9b1844
zxjzxj0010 <63140714+zxjzxj0010@users.noreply.github.com>
2026-03-06 01:38:48 +0000

    Add Baidu/IndexNow deploy
    Enable active URL submissions and normalize post metadata.
    
    - CI: run `hexo deploy` in autodeploy workflow.
    - Config: add baidu_url_submit and hexo_indexnow settings and enable corresponding deploy plugins; commented out existing git deploy block.
    - Packages: add `hexo-baidu-url-submit` and `hexo-indexnow` to package.json (package-lock updated).
    - Script: replace Baidu push script with Toutiao push script in source/js/baidu_push.js.
    - Content: update multiple post front-matter entries (set `abbrlink` values, normalize categories/tags formatting and quote cover URLs).
    
    These changes wire up automatic submission to search/index services and clean up post metadata for consistent generation and deployment. 由Github操作更新

--- js/baidu_push.js
+++ js/baidu_push.js
@@ -1,15 +1,8 @@
-// 百度自动推送脚本
+// 今日头条自动推送脚本
 (function(){
-    // 创建script标签
-    var bp = document.createElement('script');
-    // 根据当前协议选择对应的推送脚本地址
-    var curProtocol = window.location.protocol.split(':')[0];
-    if (curProtocol === 'https') {
-        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
-    } else {
-        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
-    }
-    // 插入到页面第一个script标签前面
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(bp, s);
-})();
+var el = document.createElement("script");
+el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?b0009362b58aaa3f70c2cd87e17f1a4e2d0d7dab3dc7c406028f95003f2fe34565e0a2ada1d5e86b11e7de7c1a83287d04743a02fd1ee8dd8558a8cad50e91cb354f8c6f3f78e5fd97613c481f678e6d";
+el.id = "ttzz";
+var s = document.getElementsByTagName("script")[0];
+s.parentNode.insertBefore(el, s);
+})(window);
